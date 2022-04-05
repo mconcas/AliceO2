48d0dbb10a6ca6b17f16bbfd33f06e5033da5957
Matteo Concas <matteo.concas@cern.ch>
2022-04-05 08:54:29 +0200

    Remove StandaloneDebugger

--- Detectors/ITSMFT/ITS/tracking/src/TimeFrame.cxx
+++ Detectors/ITSMFT/ITS/tracking/src/TimeFrame.cxx
@@ -124,7 +124,6 @@ int TimeFrame::loadROFrameData(gsl::span<o2::itsmft::ROFRecord> rofs,
                                const itsmft::TopologyDictionary* dict,
                                const dataformats::MCTruthContainer<MCCompLabel>* mcLabels)
 {
-  LOGP(info, ">>> Loading data into TimeFrame");
   GeometryTGeo* geom = GeometryTGeo::Instance();
   geom->fillMatrixCache(o2::math_utils::bit2Mask(o2::math_utils::TransformType::T2L, o2::math_utils::TransformType::L2G));
 
