4f0658e61da16da519b2d7ea34de8e1ef1b0c9e4
Matteo Concas <matteo.concas@cern.ch>
2022-07-02 17:50:05 +0200

    Add Cell finder on GPU, to be validated

--- Detectors/ITSMFT/ITS/tracking/include/ITStracking/Tracker.h
+++ Detectors/ITSMFT/ITS/tracking/include/ITStracking/Tracker.h
@@ -73,7 +73,7 @@ class Tracker
   std::vector<TrackITSExt>& getTracks();
 
   void setCorrType(const o2::base::PropagatorImpl<float>::MatCorrType& type) { mCorrType = type; }
-  void setParameters(const std::vector<MemoryParameters>&, const std::vector<TrackingParameters>&);
+  void setParameters(const std::vector<TrackingParameters>&);
   void getGlobalConfiguration();
   bool isMatLUT() const { return o2::base::Propagator::Instance()->getMatLUT() && (mCorrType == o2::base::PropagatorImpl<float>::MatCorrType::USEMatCorrLUT); }
   // GPU-specific interfaces
@@ -106,7 +106,6 @@ class Tracker
   TrackerTraits* mTraits = nullptr; /// Observer pointer, not owned by this class
   TimeFrame* mTimeFrame = nullptr;  /// Observer pointer, not owned by this class
 
-  std::vector<MemoryParameters> mMemParams;
   std::vector<TrackingParameters> mTrkParams;
 
   int mNThreads = 1;
@@ -120,9 +119,8 @@ class Tracker
   unsigned int mNumberOfRuns{0};
 };
 
-inline void Tracker::setParameters(const std::vector<MemoryParameters>& memPars, const std::vector<TrackingParameters>& trkPars)
+inline void Tracker::setParameters(const std::vector<TrackingParameters>& trkPars)
 {
-  mMemParams = memPars;
   mTrkParams = trkPars;
 }
 
--- Detectors/ITSMFT/ITS/tracking/include/ITStracking/TrackerTraits.h
+++ Detectors/ITSMFT/ITS/tracking/include/ITStracking/TrackerTraits.h
@@ -62,7 +62,7 @@ class TrackerTraits
     mChain = chain;
   }
 
-  virtual void initialiseTimeFrame(const int iteration, const MemoryParameters& memParams, const TrackingParameters& trackingParams);
+  virtual void initialiseTimeFrame(const int iteration, const TrackingParameters& trackingParams);
   virtual void computeLayerTracklets();
   virtual void computeLayerCells();
   virtual void refitTracks(const std::vector<std::vector<TrackingFrameInfo>>&, std::vector<TrackITSExt>&);
@@ -98,9 +98,9 @@ inline const int4 TrackerTraits::getBinsRect(const Cluster& currentCluster, int 
   return getBinsRect(layerIndex, currentCluster.phi, maxdeltaphi, z1, z2, maxdeltaz);
 }
 
-inline void TrackerTraits::initialiseTimeFrame(const int iteration, const MemoryParameters& memParams, const TrackingParameters& trackingParams)
+inline void TrackerTraits::initialiseTimeFrame(const int iteration, const TrackingParameters& trackingParams)
 {
-  mTimeFrame->initialise(iteration, memParams, trackingParams, 7);
+  mTimeFrame->initialise(iteration, trackingParams, 7);
   setIsGPU(false);
 }
 
