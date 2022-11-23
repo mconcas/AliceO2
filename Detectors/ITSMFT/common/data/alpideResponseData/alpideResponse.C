d787de63f3d42af86c2ec033c01f83cd496dd246
Matteo Concas <matteo.concas@cern.ch>
2022-11-23 19:45:41 +0100

    stash

--- Detectors/ITSMFT/common/data/alpideResponseData/alpideResponse.C
+++ Detectors/ITSMFT/common/data/alpideResponseData/alpideResponse.C
@@ -6,21 +6,17 @@
 #include <fstream>
 #include <iostream>
 
-void alpideResponse()
+void alpideResponse(const string path = "./")
 {
-    o2::itsmft::AlpideSimResponse resp0, resp1;
 
-    std::string outputPath = "$O2_ROOT/share/Detectors/ITSMFT/data/alpideResponseData/";
-    std::string mkdirCommand = "mkdir -p " + outputPath;
-    gSystem->Exec(mkdirCommand.data());
-    std::string responseFile = outputPath + "AlpideResponseData.root";
+  o2::itsmft::AlpideSimResponse resp0, resp1;
+  std::string responseFile = "AlpideResponseData.root";
 
-    std::string dataPath = "/home/abigot/alice/O2/Detectors/ITSMFT/common/data/alpideResponseData/";
-    resp0.initData(0, dataPath.data());
-    resp1.initData(1, dataPath.data());
+  resp0.initData(0, path.data());
+  resp1.initData(1, path.data());
 
-    auto file = TFile::Open(responseFile.data(), "new");
-    file->WriteObjectAny(&resp0,"o2::itsmft::AlpideSimResponse","response0");
-    file->WriteObjectAny(&resp1,"o2::itsmft::AlpideSimResponse","response1");
-    file->Close();
+  auto file = TFile::Open(responseFile.data(), "recreate");
+  file->WriteObjectAny(&resp0, "o2::itsmft::AlpideSimResponse", "response0");
+  file->WriteObjectAny(&resp1, "o2::itsmft::AlpideSimResponse", "response1");
+  file->Close();
 }
