4e817a6b0b1a62a7cf27293f1858e1855a5f5b84
Matteo Concas <matteo.concas@cern.ch>
2023-08-25 18:25:49 +0200

    Bug is reproducible

--- Detectors/ITSMFT/ITS/tracking/src/TrackerTraits.cxx
+++ Detectors/ITSMFT/ITS/tracking/src/TrackerTraits.cxx
@@ -562,6 +562,9 @@ void TrackerTraits::findTracks()
     const auto& cluster3_tf = mTimeFrame->getTrackingFrameInfoOnLayer(lastCellLevel + 2).at(clusters[lastCellLevel + 2]);
 
     TrackITSExt temporaryTrack{mTimeFrame->getCellSeeds()[lastCellLevel][lastCellIndex]};
+    if (ri < 5) {
+      temporaryTrack.print();
+    }
     temporaryTrack.setChi2(mTimeFrame->getCellSeedsChi2()[lastCellLevel][lastCellIndex]);
     for (size_t iC = 0; iC < clusters.size(); ++iC) {
       temporaryTrack.setExternalClusterIndex(iC, clusters[iC], clusters[iC] != constants::its::UnusedIndex);
